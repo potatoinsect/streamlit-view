d31d9022ae37b3cef37e324d1365673952a8ab9d
potatoinsect <123314350+potatoinsect@users.noreply.github.com>
2023-01-24 13:34:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,14 @@ st.bar_chart(view)
 
 
 
-data = pd.read_excel("C:\\Users\\jason\\OneDrive\\바탕 화면\\1학년\\교과목\\수학\\ddim excel복사본.xlsx")
-x_data = []
-y_data = []
-for i, rows in data.iterrows():
-    x_data.append( rows['greengas'])
-for i, rows in data.iterrows():
-    y_data.append( rows['electric'])
-
-data = {'greengas':x_data,
-        'electric':y_data}
-chart_data = pd.DataFrame(data, columns = ['greengas','electric'])
-
-st.write('# greengas&electric')
-st.line_chart(chart_data)
+uploaded_file = st.file_uploader("C:\\Users\\jason\\OneDrive\\바탕 화면\\1학년\\교과목\\수학\\ddim excel복사본.xlsx")
+if uploaded_file is  not None:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+    Stringio = StringIo(uploaded_file.getvalue().decode("utf-8"))
+    st.write(Stringio)
+    string_data = Stringio.read()
+    st.write(string_data)
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+    
