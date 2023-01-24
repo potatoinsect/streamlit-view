66a3cc7493a7466680e0a3001bf8a321385dedd4
potatoinsect <123314350+potatoinsect@users.noreply.github.com>
2023-01-24 13:36:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.bar_chart(view)
 
 
 
-uploaded_file = st.file_uploader("C:\\Users\\jason\\OneDrive\\바탕 화면\\1학년\\교과목\\수학\\ddim excel복사본.xlsx")
+uploaded_file = st.file_uploader("greengas")
 if uploaded_file is  not None:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
